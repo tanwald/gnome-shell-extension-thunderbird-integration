60b465a4d5cedec022c40b28ae8c8b17ab2dc183
Paul Neulinger <paul.neulinger@tanwald.net>
2011-08-04 18:13:53 +0200

    modified install script

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ TBEXT=gnome-shell-integration\@tanwald.net
 GSEXT=thunderbird-integration\@tanwald.net
 
 for FOLDER in $(cat $TBFOLDER/profiles.ini | grep Path=); do
-	TBEXTFOLDER=$TBFOLDER/${FOLDER#Path=}/extensions
-	echo "cp -rv $TBEXT $TBEXTFOLDER"
-	echo "chmod 755 $TBEXTFOLDER/$TBEXT/chrome/content/gnome-shell-integration.py"
+    TBEXTFOLDER=$TBFOLDER/${FOLDER#Path=}/extensions
+    cp -rv $TBEXT $TBEXTFOLDER
+    chmod 755 $TBEXTFOLDER/$TBEXT/chrome/content/gnome-shell-integration.py
 done
 
-echo "cp -rv $GSEXT $GSFOLDER"
+cp -rv $GSEXT $GSFOLDER
 
 echo -e "\nDONE. You might need to (re)start Thunderbird with the -purgecaches flag.\n" 
